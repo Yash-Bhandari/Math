03ae079245e7a69b0eb4cc2c9bb6d0f89ec8b8f6
Yash-Bhandari <y.bhandari1@share.epsb.ca>
2018-09-23 22:54:33 -0600

    working on linked list

--- src/Polynomial.java
+++ src/Polynomial.java
@@ -1,24 +1,34 @@
 import java.util.LinkedList;
 
 public class Polynomial {
-    LinkedList<Double> terms;
+    private LinkedList<Double> terms;
+    private int degree;
 
     public Polynomial() {
         terms = new LinkedList<Double>();
     }
 
     public void addTerm(double coefficient, int degree) {
+        if (this.degree < degree) {
+            this.degree = degree;
+        }
         terms.add(degree, coefficient);
+
     }
 
     public String toString() {
         String s = "";
         for (int i = terms.size() - 1; i >= 0; i--) {
             if (terms.get(i) != 0) {
-                if (i < terms.size() - 1)
-                    s += " +";
-                if (terms.get(i) != 1)
-                    String.valueOf(terms.get(i));
+                boolean plusAdded = false;
+                if (i < terms.size() - 1) {
+                    s += " ";
+                }
+                if (terms.get(i) != 1 || i == 0) {
+                    if (!plusAdded)
+                        s += "+ ";
+                    s += String.valueOf(terms.get(i));
+                }
                 if (i != 0)
                     s += "x^" + i;
             }
@@ -31,6 +41,7 @@ public class Polynomial {
         p.addTerm(4, 0);
         p.addTerm(3, 1);
         p.addTerm(1, 2);
+        p.addTerm(2, 7);
         System.out.println(p);
     }
 }
